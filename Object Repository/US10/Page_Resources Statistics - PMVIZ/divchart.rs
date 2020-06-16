552a1ba6dc8eda0cda401a5e86a2d3ccb2fe4f66
Rafaela Almeida <50708203+rafsalmeida@users.noreply.github.com>
2020-06-16 17:45:44 +0100

    US10 - Estatísticas de esforço testes

---
+++ Object Repository/US10/Page_Resources Statistics - PMVIZ/divchart.rs
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>divchart</name>
+   <tag></tag>
+   <elementGuidId>4784260d-1d66-4189-9ad7-f99ea6587318</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='wrapper']/div[3]/div/div/div/div[2]/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-box</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        
+                            tokyolisboauser1nairobi0100200300400500600Work time in minutesWork time in Minuteskendo.syncReady(function(){jQuery(&quot;#chart&quot;).kendoChart({&quot;categoryAxis&quot;:[{&quot;line&quot;:{&quot;visible&quot;:true},&quot;name&quot;:&quot;series-axis&quot;}],&quot;chartArea&quot;:{&quot;height&quot;:210},&quot;legend&quot;:{&quot;position&quot;:&quot;top&quot;},&quot;pannable&quot;:{&quot;lock&quot;:&quot;y&quot;},&quot;series&quot;:[{&quot;categoryField&quot;:&quot;Username&quot;,&quot;field&quot;:&quot;WorkHoursMillis&quot;,&quot;name&quot;:&quot;Work time in Minutes&quot;,&quot;type&quot;:&quot;column&quot;}],&quot;title&quot;:{&quot;text&quot;:&quot;Work time in minutes&quot;},&quot;tooltip&quot;:{&quot;format&quot;:&quot;{0}%&quot;,&quot;template&quot;:&quot;#= series.name #: #= value # minutes&quot;,&quot;visible&quot;:true},&quot;zoomable&quot;:{&quot;mousewheel&quot;:{&quot;lock&quot;:&quot;y&quot;},&quot;selection&quot;:{&quot;lock&quot;:&quot;y&quot;}},&quot;dataSource&quot;:{&quot;schema&quot;:{&quot;model&quot;:{&quot;fields&quot;:{&quot;Username&quot;:{&quot;type&quot;:&quot;string&quot;},&quot;WorkHoursMillis&quot;:{&quot;type&quot;:&quot;number&quot;},&quot;WorkHours&quot;:{&quot;type&quot;:&quot;object&quot;}}}},&quot;data&quot;:[{&quot;Username&quot;:&quot;tokyo&quot;,&quot;WorkHoursMillis&quot;:540.0,&quot;WorkHours&quot;:{&quot;Days&quot;:0.0,&quot;Hours&quot;:9.0,&quot;Minutes&quot;:0.0,&quot;Seconds&quot;:0.0,&quot;Millis&quot;:0.0}},{&quot;Username&quot;:&quot;lisboa&quot;,&quot;WorkHoursMillis&quot;:395.0,&quot;WorkHours&quot;:{&quot;Days&quot;:0.0,&quot;Hours&quot;:6.0,&quot;Minutes&quot;:35.0,&quot;Seconds&quot;:0.0,&quot;Millis&quot;:0.0}},{&quot;Username&quot;:&quot;user1&quot;,&quot;WorkHoursMillis&quot;:349.4991,&quot;WorkHours&quot;:{&quot;Days&quot;:0.0,&quot;Hours&quot;:5.0,&quot;Minutes&quot;:49.0,&quot;Seconds&quot;:29.0,&quot;Millis&quot;:946.0}},{&quot;Username&quot;:&quot;nairobi&quot;,&quot;WorkHoursMillis&quot;:45.0,&quot;WorkHours&quot;:{&quot;Days&quot;:0.0,&quot;Hours&quot;:0.0,&quot;Minutes&quot;:45.0,&quot;Seconds&quot;:0.0,&quot;Millis&quot;:0.0}}]}});});
+                        
+
+
+                </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;wrapper&quot;)/div[@class=&quot;content-page&quot;]/div[@class=&quot;content&quot;]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col&quot;]/div[@class=&quot;card-box&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='wrapper']/div[3]/div/div/div/div[2]/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Submit'])[1]/following::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Choose the workstation:'])[1]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/div/div[2]/div</value>
+   </webElementXpaths>
+</WebElementEntity>
