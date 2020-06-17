7c3dd9b963a2f09d81056cf91b3bbbb8001b627d
Rafaela Almeida <50708203+rafsalmeida@users.noreply.github.com>
2020-06-17 22:18:56 +0100

    US12 - Estatisticas de desempenho das estações testes

---
+++ Include/scripts/groovy/us12/FrequencyChartStepDefs.groovy
@@ -0,0 +1,76 @@
+package us12
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testdata.TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import internal.GlobalVariable
+
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By
+
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObjectProperty
+
+import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
+import com.kms.katalon.core.util.KeywordUtil
+
+import com.kms.katalon.core.webui.exception.WebElementNotFoundException
+
+import cucumber.api.java.en.And
+import cucumber.api.java.en.Given
+import cucumber.api.java.en.Then
+import cucumber.api.java.en.When
+
+import com.kms.katalon.core.webui.common.WebUiCommonHelper as WebUiCommonHelper
+import org.junit.Test as Test
+import org.openqa.selenium.By as By
+import org.openqa.selenium.JavascriptExecutor as JavascriptExecutor
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
+import org.openqa.selenium.chrome.ChromeDriver as ChromeDriver
+import org.openqa.selenium.support.ui.Select as Select
+
+class FrequencyChartStepDefs {
+	
+	@When("I click on the Workstations button")
+	public void i_click_on_the_Workstations_button() {
+	    WebUI.click(findTestObject('Object Repository/US12/Page_Recursos - PMVIZ/input_Utilizadores_typeR'))
+
+	}
+	
+	@When("I choose an activity with workstations events")
+	public void i_choose_an_activity_with_workstations_events() {
+	    new Select(WebUiCommonHelper.findWebElement(findTestObject('Object Repository/US12/Page_Recursos - PMVIZ/select_Escolha uma atividade               _1acae4'), 
+    2)).selectByValue('4')
+	}
+	
+	@Then("I see the frequency chart")
+	public void i_see_the_frequency_chart() {
+	    WebUI.verifyElementClickable(findTestObject('Object Repository/US12/Page_Recursos - PMVIZ/button_Mediana'))
+
+		WebUI.verifyElementClickable(findTestObject('Object Repository/US12/Page_Recursos - PMVIZ/button_Mdia'))
+		
+		WebUI.closeBrowser()
+	}
+}
---
+++ Object Repository/US12/Page_Recursos - PMVIZ/div_Show 102550100 entriesSearch           _4d35be.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Show 102550100 entriesSearch           _4d35be</name>
+   <tag></tag>
+   <elementGuidId>793167b4-e212-4b93-afcd-13fdd13cf15e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='wrapper']/div[3]/div/div/div[2]/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                    
+                        Show 102550100 entriesSearch:
+                                FrequênciaMédiaMedianaDuração MínimaDuração MáximaWorkstation
+                                FrequênciaMédiaMedianaDuração MínimaDuração MáximaWorkstation
+                            
+                            
+
+                                    
+                                    
+                                    
+                                    
+
+                            
+                                        2
+                                        59s 521ms 
+                                        59s 521ms 
+                                        30s 716ms 
+                                        1m 28s 325ms 
+                                        F2
+                                    
+                                        2
+                                        21s 684ms 
+                                        21s 684ms 
+                                        12s 979ms 
+                                        30s 388ms 
+                                        F3
+                                    
+                                        2
+                                        55s 894ms 
+                                        55s 894ms 
+                                        23s 56ms 
+                                        1m 28s 731ms 
+                                        F4
+                                    
+                                        3
+                                        17m 38s 246ms 
+                                        26s 5ms 
+                                        14s 976ms 
+                                        52m 13s 757ms 
+                                        F1
+                                    
+                        Showing 1 to 4 of 4 entries1
+                    
+            </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;wrapper&quot;)/div[@class=&quot;content-page&quot;]/div[@class=&quot;content&quot;]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='wrapper']/div[3]/div/div/div[2]/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Média em Minutos'])[1]/following::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Média'])[2]/following::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/div[2]/div</value>
+   </webElementXpaths>
+</WebElementEntity>
