cae0b535189fc647328d5888edb9461a3ab4787f
Rafaela Almeida <50708203+rafsalmeida@users.noreply.github.com>
2020-06-18 14:11:57 +0100

    US13 - estatisticas workstations work time tests

---
+++ Object Repository/US13/Page_Recursos - PMVIZ/div_No workstations associated to that activity.rs
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_No workstations associated to that activity</name>
+   <tag></tag>
+   <elementGuidId>10099d41-411b-4f73-814f-e7c21f8606ae</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='wrapper']/div[3]/div/div/div/div/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>alert alert-danger</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>alert</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                    No workstations associated to that activity
+                </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;wrapper&quot;)/div[@class=&quot;content-page&quot;]/div[@class=&quot;content&quot;]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-md-3 col-xl-3&quot;]/div[@class=&quot;card-box&quot;]/div[@class=&quot;alert alert-danger&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='wrapper']/div[3]/div/div/div/div/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Estatísticas de Recursos'])[1]/following::div[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Grafo Conformidade'])[1]/following::div[8]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Escolha um tipo de recurso:'])[1]/preceding::div[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Utilizadores'])[1]/preceding::div[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='No workstations associated to that activity']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/div/div/div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/US13/Page_Recursos - PMVIZ/divchart.rs
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>divchart</name>
+   <tag></tag>
+   <elementGuidId>560d53d2-cb4f-43de-ab52-78222b73d52d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='wrapper']/div[3]/div/div/div/div[2]/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-box</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        
+                            F1F2F4F30102030405060Tempo OperacionalTempo Operacional em Minutoskendo.syncReady(function(){jQuery(&quot;#chart&quot;).kendoChart({&quot;categoryAxis&quot;:[{&quot;line&quot;:{&quot;visible&quot;:true},&quot;name&quot;:&quot;series-axis&quot;}],&quot;chartArea&quot;:{&quot;height&quot;:210},&quot;legend&quot;:{&quot;position&quot;:&quot;top&quot;},&quot;pannable&quot;:{&quot;lock&quot;:&quot;y&quot;},&quot;series&quot;:[{&quot;categoryField&quot;:&quot;WorkstationName&quot;,&quot;field&quot;:&quot;OperationalHoursMillis&quot;,&quot;name&quot;:&quot;Tempo Operacional em Minutos&quot;,&quot;type&quot;:&quot;column&quot;}],&quot;title&quot;:{&quot;text&quot;:&quot;Tempo Operacional&quot;},&quot;tooltip&quot;:{&quot;format&quot;:&quot;{0}%&quot;,&quot;template&quot;:&quot;#= series.name #: #= value # minutos&quot;,&quot;visible&quot;:true},&quot;zoomable&quot;:{&quot;mousewheel&quot;:{&quot;lock&quot;:&quot;y&quot;},&quot;selection&quot;:{&quot;lock&quot;:&quot;y&quot;}},&quot;dataSource&quot;:{&quot;schema&quot;:{&quot;model&quot;:{&quot;fields&quot;:{&quot;WorkstationName&quot;:{&quot;type&quot;:&quot;string&quot;},&quot;OperationalHoursMillis&quot;:{&quot;type&quot;:&quot;number&quot;},&quot;OperationalHours&quot;:{&quot;type&quot;:&quot;object&quot;}}}},&quot;data&quot;:[{&quot;WorkstationName&quot;:&quot;F1&quot;,&quot;OperationalHoursMillis&quot;:52.9123,&quot;OperationalHours&quot;:{&quot;Days&quot;:0.0,&quot;Hours&quot;:0.0,&quot;Minutes&quot;:52.0,&quot;Seconds&quot;:54.0,&quot;Millis&quot;:738.0}},{&quot;WorkstationName&quot;:&quot;F2&quot;,&quot;OperationalHoursMillis&quot;:1.9840166666666668,&quot;OperationalHours&quot;:{&quot;Days&quot;:0.0,&quot;Hours&quot;:0.0,&quot;Minutes&quot;:1.0,&quot;Seconds&quot;:59.0,&quot;Millis&quot;:41.0}},{&quot;WorkstationName&quot;:&quot;F4&quot;,&quot;OperationalHoursMillis&quot;:1.8631166666666668,&quot;OperationalHours&quot;:{&quot;Days&quot;:0.0,&quot;Hours&quot;:0.0,&quot;Minutes&quot;:1.0,&quot;Seconds&quot;:51.0,&quot;Millis&quot;:787.0}},{&quot;WorkstationName&quot;:&quot;F3&quot;,&quot;OperationalHoursMillis&quot;:0.7227833333333333,&quot;OperationalHours&quot;:{&quot;Days&quot;:0.0,&quot;Hours&quot;:0.0,&quot;Minutes&quot;:0.0,&quot;Seconds&quot;:43.0,&quot;Millis&quot;:367.0}}]}});});
+                        
+
+
+                </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;wrapper&quot;)/div[@class=&quot;content-page&quot;]/div[@class=&quot;content&quot;]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col&quot;]/div[@class=&quot;card-box&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='wrapper']/div[3]/div/div/div/div[2]/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Submit'])[1]/following::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Escolha uma Estação:'])[1]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/div/div[2]/div</value>
+   </webElementXpaths>
+</WebElementEntity>
